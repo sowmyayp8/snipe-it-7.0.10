5b47addf74d08eb07a685db0aebb2b9dfcde463c
sowmyayp8 <sowmyayp8@gmail.com>
2024-10-06 16:05:13 +0530

    test

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v7.0.10',
+  'full_app_version' => 'v7.0.10 - build 14684-gc2bcc2e2d',
+  'build_version' => '14684',
+  'prerelease_version' => '',
+  'hash_version' => 'gc2bcc2e2d',
+  'full_hash' => 'v7.0.10-311-gc2bcc2e2d',
+  'branch' => 'master',
+);
